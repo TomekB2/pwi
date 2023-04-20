f8e96171edb40b58508268764babfb90363fb04c
TomekB2 <126956626+TomekB2@users.noreply.github.com>
2023-04-20 08:13:26 +0200

    Update script.js

--- sparwd/script.js
+++ sparwd/script.js
@@ -1,16 +1,13 @@
-    // Skrypt obsługujący nawigację w SPA
-    const navLinks = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll('nav a');
 
     navLinks.forEach(link => {
       link.addEventListener('click', (event) => {
-        event.preventDefault(); // Zatrzymujemy domyślne zachowanie kliknięcia
-
-        const target = event.target.getAttribute('href'); // Pobieramy atrybut href z klikniętego linka
-        const section = document.querySelector(target); // Znajdujemy sekcję o danym identyfikatorze
-
+        event.preventDefault(); 
+        const target = event.target.getAttribute('href');
+        const section = document.querySelector(target); 
         window.scrollTo({
-          top: section.offsetTop, // Ustawiamy pozycję przewijania na początek sekcji
-          behavior: 'smooth' // Dodajemy efekt płynnego przewijania
+          top: section.offsetTop, 
+          behavior: 'smooth'
         });
       });
     });
